792211dcaf0c5f237158f2ecfc5bf7127f44f111
Rohit3kushwah <rk6109744@gmail.com>
2025-08-29 16:12:19 -0500

    add

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "https://rohit-portfolio-server.onrender.com/api",
+  baseURL: import.meta.env.VITE_API_URL || "https://rohit-portfolio-server.onrender.com",
   withCredentials: true, // IMPORTANT for httpOnly cookie
 });
 
